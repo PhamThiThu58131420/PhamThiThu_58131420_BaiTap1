ca0bd0eb3fb2c76afb6e63115b0454665131027b
phamthu <phamthu@192.168.231.1>
2020-04-12 10:01:28 +0700

    V1.0

---
+++ src/phamthithu_58131420_baitap1/GiaoVien.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phamthithu_58131420_baitap1;
+
+/**
+ *
+ * @author phamthu
+ */
+public class GiaoVien 
+{
+    String monDay;
+    String toBoMon;
+
+    public GiaoVien(String monDay, String toBoMon) {
+        this.monDay = monDay;
+        this.toBoMon = toBoMon;
+    }
+
+    public String getMonDay() {
+        return monDay;
+    }
+
+    public void setMonDay(String monDay) {
+        this.monDay = monDay;
+    }
+
+    public String getToBoMon() {
+        return toBoMon;
+    }
+
+    public void setToBoMon(String toBoMon) {
+        this.toBoMon = toBoMon;
+    }
+    
+    
+}
---
+++ src/phamthithu_58131420_baitap1/HocSinh.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phamthithu_58131420_baitap1;
+
+/**
+ *
+ * @author phamthu
+ */
+public class HocSinh 
+{
+    String lop;
+    String nangKhieu;
+
+    public HocSinh(String lop, String nangKhieu) {
+        this.lop = lop;
+        this.nangKhieu = nangKhieu;
+    }
+
+    public String getLop() {
+        return lop;
+    }
+
+    public void setLop(String lop) {
+        this.lop = lop;
+    }
+
+    public String getNangKhieu() {
+        return nangKhieu;
+    }
+
+    public void setNangKhieu(String nangKhieu) {
+        this.nangKhieu = nangKhieu;
+    }
+    
+    
+    
+}
